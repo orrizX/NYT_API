c76cec8b6ef21329a4da5bcb1eb372898fe68b78
orrizX <zo292@nyu.edu>
2021-11-22 19:38:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,65 @@
+"use strict";
+var data;
 
+//get data
+var myRequestObject = new XMLHttpRequest();
+var url = "https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=rK5JyGGqZnGLztfHkUcecSp9Y5GUVbsQ";
+myRequestObject.onreadystatechange = function() {
+  if (myRequestObject.readyState == 4 && myRequestObject.status == 200) {
 
-var app = {
-	nyTimesArticles : [],
+    data = JSON.parse(myRequestObject.responseText);
+    displayPage();
 
-	initialize: function() {
-		app.getNYTimesData();
-	},
-
-	makeHTML: function() {
-		var theHTML = '';
-		for (var i = 0; i < app.nyTimesArticles.length; i++){
-			theHTML += "<div class='nytArticle'>";
-			theHTML += "<h3>" + app.nyTimesArticles[i].headline.main + "</h3>";
-			theHTML += "</div>";
-		}
-		$('.container').html(theHTML);
-	},
-
-	getNYTimesData: function() {
-		console.log("Get NY Times Data");
-		var currentSearchWord = 'biden';
-		var nyTimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + currentSearchWord + '&page=0&sort=newest&api-key=';
-		var myNYKey = 'E0Jowl0MxvI4RRWnjChL1eFDPwhbj3EE';
-		var nyTimesReqURL = nyTimesURL + myNYKey;
-		console.log(nyTimesReqURL);
-		fetch(nyTimesReqURL)
-		.then(response => response.json())
-		.then(data => {
-			debugger;
-			app.nyTimesArticles = data.response.docs;
-			console.log(app.nyTimesArticles);
-			app.makeHTML();
-		})
-		.catch(error => console.log(error));
-	}
+  }
 };
+myRequestObject.open("GET", url);
+myRequestObject.send();
+
+
+// display the page 
+
+function displayPage() {
+
+  var headline = document.createElement('span');
+  var headlineText = document.createTextNode("Here's what's in the news week: ");
+  headline.appendChild(headlineText);
+  document.body.appendChild(headline);
+  headline.style.fontSize = "1.7em";
+  headline.style.fontWeight = "bold";
+  headline.style.color = "rgb(30,30,30)"
+
+  var todaysDate = document.createElement('span');
+  var todaysDateText = document.createTextNode(new Date().toString().substr(0, 15));
+  todaysDate.appendChild(todaysDateText);
+  document.body.appendChild(todaysDate);
+  todaysDate.style.fontSize = "1.7em";
+  todaysDate.style.fontWeight = "bold";
+  todaysDate.style.color = "rgb(159,182,205)";
+
+  var list = document.createElement('ol');
+  for (let i = 0; i < 10; i++) {
+    let li = document.createElement('li');
+    li.style.fontSize = "1.1em";
+    li.style.color = "rgb(30,30,30)";
+    list.appendChild(li);
+  }
+  document.body.appendChild(list);
+
+  function displayList() {
+    // console.log('displayList is run.');
+    var lis = document.getElementsByTagName('li');
+    for (let i = 0; i < lis.length; i++) {
+      // var li = document.createElement('li');
+      let a = document.createElement('a')
+      a.setAttribute('href', 'data.results[i].url');
+      lis[i].appendChild(a);
+      let aText = document.createTextNode(data.results[i].title);
+      a.appendChild(aText)
+
+    }
+
+  }
+  displayList();
+
+}
+
